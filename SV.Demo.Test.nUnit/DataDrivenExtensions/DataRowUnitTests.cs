848d45961fdf0008890c9906a1532b1d6874aa1c
Michiel Doeven <m.doeven@sharevalue.nl>
2022-04-29 23:18:33 +0200

    DataRowUnitTests

---
+++ SV.Demo.Test.nUnit/DataDrivenExtensions/DataRowUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace SV.Demo.Test.MSTestV2.DataDrivenExtensions;
+
+[TestFixture]
+public class DataRowUnitTests
+{
+	[Test]
+	[TestCase(1, 2, 3)]
+	[TestCase(2, 5, 7)]
+	[TestCase(4, 6, 10)]
+	public void DataRowAddTest(int a, int b, int expected)
+	{
+		int actual = Calculator.Add(a, b);
+		actual.Should().Be(expected);
+	}
+}
