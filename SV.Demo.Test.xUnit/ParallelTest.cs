2eea273ea244bc72d0ed4cd8dfcb809673650702
Michiel Doeven <m.doeven@sharevalue.nl>
2022-05-19 11:16:20 +0200

    xUnit Parallel en Debug only optie.

--- SV.Demo.Test.xUnit/ParallelTest.cs
+++ SV.Demo.Test.xUnit/ParallelTest.cs
@@ -2,7 +2,48 @@ using System.Threading;
 
 namespace SV.Demo.Test.xUnit;
 
+[CollectionDefinition("Papallel Collection", DisableParallelization = false)]
 public class ParallelTest
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        Thread.Sleep(3000);
+    }
+
+    [Fact]
+    public void TestMethod2()
+    {
+        Thread.Sleep(3000);
+    }
+
+    [RunnableInDebugOnly]
+    public void TestMethod3()
+    {
+        Thread.Sleep(3000);
+    }
+
+    [Fact]
+    public void TestMethod4()
+    {
+        Thread.Sleep(3000);
+    }
+
+    [Fact]
+    public void TestMethod5()
+    {
+        Thread.Sleep(3000);
+    }
+
+    [Fact]
+    public void TestMethod6()
+    {
+        Thread.Sleep(3000);
+    }
+
+}
+
+public class ParallelTest2
 {
     [Fact]
     public void TestMethod1()
@@ -27,4 +68,61 @@ public class ParallelTest
     {
         Thread.Sleep(3000);
     }
+
+}
+
+public class ParallelTest3
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestMethod2()
+    {
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestMethod3()
+    {
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestMethod4()
+    {
+        Thread.Sleep(2000);
+    }
+
+}
+
+public class ParallelTest4
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestMethod2()
+    {
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestMethod3()
+    {
+        Thread.Sleep(2000);
+    }
+
+    [Fact]
+    public void TestMethod4()
+    {
+        Thread.Sleep(2000);
+    }
+
 }
---
+++ SV.Demo.Test.xUnit/RunnableInDebugOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace SV.Demo.Test.xUnit;
+
+public class RunnableInDebugOnlyAttribute : FactAttribute
+{
+	public RunnableInDebugOnlyAttribute()
+	{
+		if (!Debugger.IsAttached)
+		{
+			Skip = "Only running in interactive mode.";
+		}
+	}
+}
